8406ceb6082e01ad42642ee16ac9016a3db2d55c
DivyaniThota <divyaniprasad99@gmail.com>
2024-02-05 19:43:49 -0700

    Initial Commit

---
+++ DivyaniThotaCard/DivyaniThotaCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  DivyaniThotaCardApp.swift
+//  DivyaniThotaCard
+//
+//  Created by DIVYANI PRASAD THOTA on 05/02/24.
+//
+
+import SwiftUI
+
+@main
+struct DivyaniThotaCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
